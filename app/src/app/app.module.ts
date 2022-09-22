86f871a423c6ca95bddf981c85fac6dab5cf82f2
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-09-22 16:43:58 +0300

    feat: generate app components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authorization/login/login/login.component';
+import { RegistrationComponent } from './authorization/registration/registration/registration.component';
+import { CardCreatorComponent } from './card-creator/card-creator/card-creator.component';
+import { ErrorComponent } from './error/error/error.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SettingsComponent } from './header/settings/settings.component';
+import { SearchCardComponent } from './search/cards/search-card/search-card.component';
+import { DescriptionCardComponent } from './search/cards/description-card/description-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CardCreatorComponent,
+    ErrorComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SettingsComponent,
+    SearchCardComponent,
+    DescriptionCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ app/src/app/card-creator/card-creator/card-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-creator',
+  templateUrl: './card-creator.component.html',
+  styleUrls: ['./card-creator.component.scss']
+})
+export class CardCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
