c81e0b9559f49ef1037cdb14658bdf7229467474
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-09-26 13:03:03 +0300

    refactor: convert linter config

---
+++ app/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        createDefaultProgram: true,
+      },
+      extends: [
+        'plugin:import/recommended',
+        'plugin:@angular-eslint/recommended',
+        'airbnb-typescript/base',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'prettier',
+      ],
+      rules: {
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'app',
+            style: 'camelCase',
+          },
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'app',
+            style: 'kebab-case',
+          },
+        ],
+      },
+    },
+    {
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
+  ],
+};
