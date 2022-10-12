b3dbea2a06de56d72377a240d8ff24e6cfc1ad12
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-10-12 20:29:20 +0300

    refactor: change project structure

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -10,11 +10,11 @@ import { AppComponent } from './app.component';
 import { LoginModule } from './authorization/pages/login/login/login.module';
 import { RegistrationModule } from './authorization/pages/registration/registration/registration.module';
 import { CardCreatorComponent } from './card-creator/card-creator/card-creator.component';
-import { HeaderBtnComponent } from './core/header/header-btn/header-btn.component';
-import { HeaderComponent } from './core/header/header/header.component';
-import { LoginInfoComponent } from './core/header/login-info/login-info.component';
-import { SearchInputComponent } from './core/header/search-input/search-input.component';
-import { SettingsComponent } from './core/header/settings/settings.component';
+import { HeaderBtnComponent } from './core/components/header/components/header-btn/header-btn.component';
+import { LoginInfoComponent } from './core/components/header/components/login-info/login-info.component';
+import { SearchInputComponent } from './core/components/header/components/search-input/search-input.component';
+import { SettingsComponent } from './core/components/header/components/settings/settings.component';
+import { HeaderComponent } from './core/components/header/header/header.component';
 import { ErrorModule } from './error/error/error.module';
 import { FormInputModule } from './shared/components/form-input/form-input.module';
 import { FormModule } from './shared/components/form/form.module';
@@ -23,25 +23,6 @@ import { DescriptionCardModule } from './youtube/components/description-card/des
 import { SearchCardModule } from './youtube/components/search-card/search-card.module';
 import { SearchResultsModule } from './youtube/components/search-results/search-results.module';
 import { FilterPipe } from './youtube/pipes/filter.pipe';
-// import { HeaderComponent } from './header/header/header.component';
-// import { SettingsComponent } from './header/settings/settings.component';
-// import { LoginInfoComponent } from './header/login-info/login-info.component';
-// import { SearchInputComponent } from './header/search-input/search-input.component';
-// import { MatIconModule } from '@angular/material/icon';
-// import { HeaderBtnComponent } from './header/header-btn/header-btn.component';
-// import { MatButtonModule } from '@angular/material/button';
-// import { FormsModule } from '@angular/forms';
-// import { FilterPipe } from './pipes/filter.pipe';
-// import { ErrorModule } from './error/error/error.module';
-// import { SearchResultsModule } from './search/search-results/search-results.module';
-// import { SearchCardModule } from './search/cards/search-card/search-card.module';
-// import { MatCardModule } from '@angular/material/card';
-// import { CardStatisticModule } from './search/cards/card-statistic/card-statistic.module';
-// import { DescriptionCardModule } from './search/cards/description-card/description-card.module';
-// import { LoginModule } from './authorization/login/login/login.module';
-// import { FormInputModule } from './shared/components/form-input/form-input.module';
-// import { RegistrationModule } from './authorization/registration/registration/registration.module';
-// import { FormModule } from './shared/components/form/form.module';
 
 @NgModule({
   declarations: [
