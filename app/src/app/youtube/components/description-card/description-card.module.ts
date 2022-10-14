347d6194f1995d73a6d50270b76601353b7413a6
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-10-14 12:37:16 +0300

    feat: create scroll for cards

--- app/src/app/youtube/components/description-card/description-card.module.ts
+++ app/src/app/youtube/components/description-card/description-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DescriptionCardComponent } from './description-card.component';
 import { CardStatisticModule } from '../card-statistic/card-statistic.module';
+import { DirectivesModule } from '../../directive/directives.module';
 
 @NgModule({
   declarations: [DescriptionCardComponent],
-  imports: [CommonModule, CardStatisticModule],
+  imports: [CommonModule, CardStatisticModule, DirectivesModule],
   exports: [DescriptionCardComponent],
 })
 export class DescriptionCardModule {}
