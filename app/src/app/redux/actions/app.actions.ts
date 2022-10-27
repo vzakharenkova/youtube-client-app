3d7fb8e6ebfea633584e65362a9ee866ac9b9e5c
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-10-27 15:21:38 +0300

    refactor: delete unused code

--- app/src/app/redux/actions/app.actions.ts
+++ app/src/app/redux/actions/app.actions.ts
@@ -10,8 +10,3 @@ export const updateCustomUserCards = createAction(
   '[VIDEO_LIST] Update Custom User Cards',
   props<{ customCard: CustomCardModel }>(),
 );
-
-export const filterSearchResult = createAction(
-  '[VIDEO_LIST] Filter Search Result',
-  // props<{ searchResult: VideoItem[] }>(),
-);
